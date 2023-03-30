0cdb909a1ae4c252f860d7557767de03f1e2c765
LuDev01 <luisatoro0121@hotmail.com>
2023-03-30 12:24:33 -0500

    Hice una restica de dos numeros

--- prueba.js
+++ prueba.js
@@ -1,2 +1,9 @@
 console.log("Holaa soy tu novio");
-console.log("Hola soy tu noviaaaa")
+console.log("Hola soy tu noviaaaa");
+
+const restarNumeros=function(n1,n2)
+{
+    return n1-n2
+}
+
+console.log(restarNumeros(25,13))
