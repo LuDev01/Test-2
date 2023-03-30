2a6438cc09887103c954b3ef0f8c874029b14fc2
camilozv21 <camilozv21@gmail.com>
2023-03-30 12:23:50 -0500

    Añadí la función sumar2Numeros

--- prueba.js
+++ prueba.js
@@ -1,2 +1,6 @@
 console.log("Holaa soy tu novio");
-console.log("Hola soy tu noviaaaa")
+console.log("Hola soy tu noviaaaa");
+
+const sumar2Numeros = (n1 ,n2) => n1 + n2;
+
+console.log(sumar2Numeros(2,44))
