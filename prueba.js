3e89cb4fda333e8922ef5b23243d7dae010fe9fb
LuDev01 <luisatoro0121@hotmail.com>
2023-03-30 12:49:24 -0500

    Otra pruebita

--- prueba.js
+++ prueba.js
@@ -1,16 +1,14 @@
 console.log("Holaa soy tu novio");
 console.log("Hola soy tu noviaaaa");
 
-
 const restarNumeros=function(n1,n2)
 {
     return n1-n2
 }
 
 console.log(restarNumeros(25,13))
-
 const sumar2Numeros = (n1 ,n2) => n1 + n2;
 
 console.log(sumar2Numeros(2,44))
 
-
+console.log("Camilito es el más teso de todos");
